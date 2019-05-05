0d10dc99250272328db0ca338a23e77b2d986e04
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-05-05 19:43:02 +0200

    Pending XAMLC sample files

---
+++ src/XAMLC/XAMLCPerformance/XAMLCPerformance.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XAMLCPerformance.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new XAMLCPerformance.App());
+        }
+    }
+}
