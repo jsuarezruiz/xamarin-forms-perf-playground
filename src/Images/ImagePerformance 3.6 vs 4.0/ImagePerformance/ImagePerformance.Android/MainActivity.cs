05aaefcd379379d09fed99d685543299f515bdc8
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-06-02 11:58:09 +0200

    Added another image performance comparison between XF3.6 and XF4.0

---
+++ src/Images/ImagePerformance 3.6 vs 4.0/ImagePerformance/ImagePerformance.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace ImagePerformance.Droid
+{
+    [Activity(
+        Label = "ImagePerformance", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme",
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ src/Images/ImagePerformance 3.6 vs 4.0/ImagePerformance/ImagePerformance.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ImagePerformance.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new ImagePerformance.App());
+        }
+    }
+}
